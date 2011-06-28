30cf3a56b00511123c86b61d076f8218dfaff990
Eric Ren <eric.ren@jivesoftware.com>
2011-06-28 14:21:07 -0700

    pre-alpha version of canvas view.

--- jiveapp/javascripts/main.js
+++ jiveapp/javascripts/main.js
@@ -1,29 +1,41 @@
-
+var themeAry = [ 'Commits', 'Holidays', 'Viral Vids'];
 var glObj = {};
 glObj.timer_is_on = 0;
 glObj.refreshTime = 3000;
-alert(glObj.timer_is_on); //test
+//alert(glObj.timer_is_on); //test
+
+
+// Get the size of an object
+Object.size = function(obj) {
+    var size = 0, key;
+    for (key in obj) {
+	if (obj.hasOwnProperty(key)) size++;
+    }
+    return size;
+};
 
 
 //continually request data from server
 function startBadges() {
     if (!glObj.timer_is_on) {
 	glObj.timer_is_on = 1;
-	alert("timer on: " + glObj.timer_is_on); //test
+	//	alert("timer on: " + glObj.timer_is_on); //test
 	getBadges();
 	document.getElementById('button').innerHTML = "Stop requesting";
 	
     } 
     else {
 	glObj.timer_is_on = 0;
-	alert("timer off: " + glObj.timer_is_on); //test
+	//	alert("timer off: " + glObj.timer_is_on); //test
 	clearTimeout(glObj.t);
 	document.getElementById('button').innerHTML = "Continue requesting";
     }
 }
 
-function getBadges() {
-    var url = 'http://10.45.120.176:8080/CodeSquare/FrontEndServlet?name=' + glObj.name;
+//change for testing
+function makeBadgeTable() {
+    //    var url = 'http://10.45.120.176:8080/CodeSquare/FrontEndServlet?email=' + glObj.name;
+    var url = 'http://10.45.120.176:8080/CodeSquare/FrontEndServlet?email=diivanand.ramalingam@jivesoftware.com';
     //document.getElementById('test').innerHTML += name +"<br />" + url;
     var params = {'href' : url, 'format' : 'json', 'authz' : 'none' };
     
@@ -32,17 +44,29 @@ function getBadges() {
 		alert("Error: " + response.error.message + "\nbetter debug it..."); // Deal with this...
 	    }
 	    else {
-		// use the data 
-		var jsondata = response.content;
-		var html = "";
-		for (var key in jsondata) {
-		    var value = jsondata[key];
-		    html += key + ": " + value + "<br />";
+		// get json data from response
+		var obj = response.content;
+		var key, value, 
+		    themeNum = 0,
+		    html = "";
+
+		//1. 6. 11
+		//sort the json data
+		for (key = 1; key <= Object.size(obj); key++) {
+		     value = obj[key];
+		     if (key % 5 == 1) {
+			 html += "<tr><th>" + themeAry[themeNum] + "</th>";
+			 themeNum++;
+		     }
+		     html += "<td><img src=" + value.IconURL + "/><p>" + value.name + "</p></td>";
+		     if (key % 5 == 0) {
+			 html += "</tr>";
+		     }
 		}
 	    }
 	    
-	    document.getElementById('content_div').innerHTML = html;
-	    glObj.t = setTimeout("getBadges()", glObj.refreshTime);
+	    document.getElementById('badgeTable').innerHTML = html;
+	    //glObj.t = setTimeout("getBadges()", glObj.refreshTime);
 	});
 }
 
@@ -57,11 +81,11 @@ function init() {
 		var user = viewerData.displayName;
 		glObj.user = user.replace(/\s/, '_');
 		
-		gadgets.window.adjustHeight();
+		//gadgets.window.adjustHeight();
 	    }
 	})
 }
 
 gadgets.util.registerOnLoadHandler(init);
-gadgets.window.adjustHeight();
+//gadgets.window.adjustHeight();
 
