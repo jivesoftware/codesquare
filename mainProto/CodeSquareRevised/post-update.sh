07749ec29f199a353c3bcca2797ef5d4fa42632b
Justin Kikuchi <justin.kikuchi@jivesoftware.com>
2011-07-28 17:07:59 -0700

    changed the script to post to the servlet instead of get

--- mainProto/CodeSquareRevised/post-update.sh
+++ mainProto/CodeSquareRevised/post-update.sh
@@ -3,7 +3,7 @@
 email=`git config user.email`
 beforeDate=`date`
 # get from jar
-afterDate="Wed Jul 27 11:24:24 2011 -0700"i
+afterDate="Wed Jul 20 11:24:24 2011 -0700"
 email=justin.kikuchi@jivesoftware.com
 unixTime=`date +%s`
 timeZone=`date +%z`
@@ -12,7 +12,7 @@ com=`git log --pretty=format:']"},{ "cID": "%H", "cMes": "%f", "unixtimestamp": 
 com="["${com:4}"]\"}]"; 
 x=`echo $com`
 
-`curl -v --get --data-urlencode "timeZone=$timeZone" --data-urlencode "unixTime=$unixTime" --data-urlencode "json=$x" http://10.45.111.143:9090/CodeSquareTesting/BackEndServlet`
+`curl -v --data-urlencode "timeZone=$timeZone" --data-urlencode "unixTime=$unixTime" --data-urlencode "json=$x" http://10.45.111.143:9090/CodeSquare/BackEndServlet`
 
-git log --pretty=format:']"},{ "cID": "%H", "cMes": "%f", "unixtimestamp": "%ct", "isotimestamp": "%ci", "name": "%cn", "email": "%ce", "stats": "[' --numstat --before="Wed Jul 27 10:46:33 PDT 2011" --after="Tue Jul 27 07:59:13 2011"  --committer=justin.kikuchi@jivesoftware.com
+#git log --pretty=format:']"},{ "cID": "%H", "cMes": "%f", "unixtimestamp": "%ct", "isotimestamp": "%ci", "name": "%cn", "email": "%ce", "stats": "[' --numstat --before="Wed Jul 27 10:46:33 PDT 2011" --after="Tue Jul 27 07:59:13 2011"  --committer=justin.kikuchi@jivesoftware.com
 
