5c9dcd9c74ffe693e9045bc4d5e808afe1d35fb3
diivanand <diivanand@gmail.com>
2011-08-01 10:26:41 -0700

    fixing caching

--- mainProto/CodeSquareRevised/CodeSquare/src/java/com/jivesoftware/appServlet/AppServlet.java
+++ mainProto/CodeSquareRevised/CodeSquare/src/java/com/jivesoftware/appServlet/AppServlet.java
@@ -159,7 +159,7 @@ public class AppServlet extends HttpServlet {
                         response.setHeader("Cache-Control","no-cache"); //HTTP 1.1
                         response.setHeader("Pragma","no-cache"); //HTTP 1.0
                         response.setDateHeader ("Expires", 0); //prevents caching at the proxy server
-                        response.setHeader("Cache-Control","no-store"); //HTTP 1.1
+                        //response.setHeader("Cache-Control","no-store"); //HTTP 1.1
                         out.write(j.toString().getBytes());
                         out.close(); // Closes the output stream
                     }
