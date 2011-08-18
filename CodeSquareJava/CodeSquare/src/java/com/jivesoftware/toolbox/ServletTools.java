41ef0578908642897cc31d1833c199d4c97b7856
Justin Kikuchi <justin.kikuchi@jivesoftware.com>
2011-08-18 11:04:41 -0700

    commented code woot woot

--- CodeSquareJava/CodeSquare/src/java/com/jivesoftware/toolbox/ServletTools.java
+++ CodeSquareJava/CodeSquare/src/java/com/jivesoftware/toolbox/ServletTools.java
@@ -23,62 +23,49 @@ import java.util.List;
  */
 public class ServletTools {
     
-         public static boolean hasParams(HttpServletRequest request, String[] params){
-             
-             for(int i=0; i<params.length;i++){
-                 if(request.getParameter(params[i]) == null){
-                     return false;
-                 }
+     public static boolean hasParams(HttpServletRequest request, String[] params){
+         for(int i=0; i<params.length;i++){
+             if(request.getParameter(params[i]) == null){
+                 return false;
              }
-             /*
-             if((request.getParameter(params[0]) != null) &&
-                     (request.getParameter(params[1]) != null) &&
-                     (request.getParameter(params[2]) != null)
-                     ) {
-             if((request.getParameter(params[0]).length() > 0) &&
-                     (request.getParameter(params[1]).length() > 0) &&
-                     (request.getParameter(params[2]).length() > 0)
-                     ) {
-                 return true;
-             }
-             }*/
-             return true;
          }
+         return true;
+     }
 	
-	/**
-	 * Sends a response back to the requester in JSON format. The data in the json depends on what the request was.
-	 * @param response the http servlet response to a given http servlet request
-	 * @param j The JSONObject to be sent as a String to the requester
-	 * @throws JSONException
-	 * @throws IOException
-	 */
-	public static void sendJSONOutput(HttpServletResponse response, JSONObject j) throws JSONException, IOException{
-            response.setContentType("application/json");
-            OutputStream out = response.getOutputStream();
-            response.setContentLength(j.toString().length());
-            out.write(j.toString().getBytes());
-            out.close(); // Closes the output stream
-	}
-	
-	/**
-	 * Converts a JSON Commit Object into  Java Commit Object
-	 * @param jCommit the JSON Object containing commit information
-	 * @param pushDate the Git Push date of the commit
-	 * @return A java Commit object
-	 * @throws JSONException
-	 */
-	public static Commit convertToCommit(JSONObject jCommit, 
-                String pushUnixTime) throws JSONException{
-            Commit commit = new Commit();
-            commit.setPushDate(new JiveDate(pushUnixTime));
-            String x = jCommit.getString("isotimestamp").split(" ")[2];
-            commit.setCommitDate(new JiveDate(jCommit.getString("unixtimestamp"), jCommit.getString("isotimestamp")));
-            commit.addStats(jCommit.getString("stats"));
-            commit.setId(jCommit.getString("cID"));
-            commit.setEmail(jCommit.getString("email"));
-            commit.setMessage(jCommit.getString("cMes"));
-            return commit;
-	}
+    /**
+     * Sends a response back to the requester in JSON format. The data in the json depends on what the request was.
+     * @param response the http servlet response to a given http servlet request
+     * @param j The JSONObject to be sent as a String to the requester
+     * @throws JSONException
+     * @throws IOException
+     */
+    public static void sendJSONOutput(HttpServletResponse response, JSONObject j) throws JSONException, IOException{
+        response.setContentType("application/json");
+        OutputStream out = response.getOutputStream();
+        response.setContentLength(j.toString().length());
+        out.write(j.toString().getBytes());
+        out.close(); // Closes the output stream
+    }
+
+    /**
+     * Converts a JSON Commit Object into  Java Commit Object
+     * @param jCommit the JSON Object containing commit information
+     * @param pushDate the Git Push date of the commit
+     * @return A java Commit object
+     * @throws JSONException
+     */
+    public static Commit convertToCommit(JSONObject jCommit, 
+            String pushUnixTime) throws JSONException{
+        Commit commit = new Commit();
+        commit.setPushDate(new JiveDate(pushUnixTime));
+        String x = jCommit.getString("isotimestamp").split(" ")[2];
+        commit.setCommitDate(new JiveDate(jCommit.getString("unixtimestamp"), jCommit.getString("isotimestamp")));
+        commit.addStats(jCommit.getString("stats"));
+        commit.setId(jCommit.getString("cID"));
+        commit.setEmail(jCommit.getString("email"));
+        commit.setMessage(jCommit.getString("cMes"));
+        return commit;
+    }
         
     /**
      * This method takes the list of badges that a person recently obtained
